5552ab137931dd756c042fc6d7496e6abd0f2196
markusritschel <git@markusritschel.de>
2021-11-20 19:01:13 +0100

    Initialize files

---
+++ DS28EA00.h
@@ -0,0 +1,15 @@
+//Control Function Commands
+#define CHAIN            0x99
+
+
+//1-Wire ROM Function Commands
+#define CONDITIONAL_READ_ROM    0x0F
+#define MATCH_ROM               0x55
+
+
+//Chain States
+#define CHAIN_OFF  0x3C
+#define CHAIN_ON   0x5A
+#define CHAIN_DONE 0x96
+
+#define VALID_SEQUENCE 0xAA
---
+++ tsticks.ino
@@ -0,0 +1,11 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "DS28EA00.h"
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+}
